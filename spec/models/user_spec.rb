ba571ddc52798035558533b73903467a5646eff2
andylinteau <andy.linteau@buystand.com>
2013-06-29 00:51:58 +0800

    adding password functionality

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,14 +13,24 @@ require 'spec_helper'
 
 describe User do
   
-  before { @user = User.new(name: "Example User", email: "user@example.com")}
+  before  @user = User.new(name: "Example User", email: "user@example.com",
+                            password: "foobar", password_confirmation: "foobar")
+  
 
   subject { @user }
   
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
   
   it { should be_valid }
+  
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
+    it { should_not be_valid }
+  end
   
   describe "when name is not present" do 
     before { @user.name = " " }
