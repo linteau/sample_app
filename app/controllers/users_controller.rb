eabe261bc4a1fb11425b958b514e0857dccd2f3f
andylinteau <andy.linteau@buystand.com>
2013-07-28 16:27:51 +0800

    added a mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,9 +25,10 @@ class UsersController < ApplicationController
     else
       @user = User.new(params[:user])
       if @user.save
+        UserMailer.welcome_email(@user).deliver
         sign_in @user
         redirect_to @user
-        flash[:success] = "Welcome to the Sample App!"
+        flash[:success] = "Welcome to the Sample App!"       
       else
         render 'new'
       end
